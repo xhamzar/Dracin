bfe45c478c2c6998c3d92e295fcd9e7882ec6171
Hamzar <60004028+xhamzar@users.noreply.github.com>
2026-02-01 06:07:31 +0700

    Update header title from 'SekaiDrama' to 'Dracin-Luc'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -91,7 +91,7 @@ export function Header() {
               <Play className="w-5 h-5 text-white fill-white" />
             </div>
             <span className="font-display font-bold text-xl gradient-text">
-              SekaiDrama
+              Dracin-Luc
             </span>
           </Link>
 
